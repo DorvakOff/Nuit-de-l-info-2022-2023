859a00a767fd996cf49a490bce311724664f190a
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-12-01 23:31:35 +0100

    add(accueil&plateau&case): Création des éléments html et css pour générer une grille

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccueilComponent} from "./accueil/accueil.component";
+import {PlateauComponent} from "./plateau/plateau.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'game', component: PlateauComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/plateau/plateau.component.ts
+++ frontend/src/app/plateau/plateau.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-plateau',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./plateau.component.css']
 })
 export class PlateauComponent {
+
+    constructor() { }
 
 }
