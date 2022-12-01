2cf01c11263344ee7d0ab4beeebf2d71cdb49fc3
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-12-01 21:07:41 +0100

    add(case): Création du composant case

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlateauComponent } from './plateau/plateau.component';
+import { CaseComponent } from './case/case.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlateauComponent
+    PlateauComponent,
+    CaseComponent
   ],
   imports: [
     BrowserModule,
