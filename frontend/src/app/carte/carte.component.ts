6f64fc89c6bf3efc1ca0bad30093801243810068
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-12-02 02:44:24 +0100

    change(carte): Mise à jour du design des cartes

--- frontend/src/app/carte/carte.component.ts
+++ frontend/src/app/carte/carte.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./carte.component.css']
 })
 export class CarteComponent {
+  public question: String = "Question";
+  public rep1: String = "Réponse 1";
+  public rep2: String = "Réponse 2";
+  public rep3: String = "Réponse 3";
+  public moreInformation: String = "Plus d'information";
+
+
+  constructor(question: String, rep1: String, rep2: String, rep3: String, moreInformation: String) {
+    this.question = question;
+    this.rep1 = rep1;
+    this.rep2 = rep2;
+    this.rep3 = rep3;
+    this.moreInformation = moreInformation;
+  }
 
 }
