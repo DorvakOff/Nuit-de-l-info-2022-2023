2f74f06e1ad4b15f8d53af9e011dcee62dc239e9
Vincent GUILLOU <vince12juju@gmail.com>
2022-12-02 03:16:29 +0100

    fix(plateau): bug modification disposition case plateau

--- frontend/src/app/plateau/plateau.component.ts
+++ frontend/src/app/plateau/plateau.component.ts
@@ -17,21 +17,6 @@ export class PlateauComponent {
   }
 
   count(nb: number = 64) {
-    let nbs: {row: number, col: number}[] = []
-    let ax = 0;
-    let ay = 0;
-    for (let i = 0; i < 8; i++) {
-
-    }
-    for (let row = 0; row < 8; row++) {
-      for (let col = 0; col < 8-row; col++) {
-
-      }
-    }
-    return nbs
-  }
-
-  getPositionNb(nb: number, nbCol: number): {x: number, y: number} {
-    return {x: 0, y: 0}
+    return Array(nb)
   }
 }
