22060398f322a056e2a60fc18c04e118cd359b40
Vincent GUILLOU <vince12juju@gmail.com>
2022-12-02 03:04:03 +0100

    feat(plateau): modification disposition case plateau

--- frontend/src/app/case/case.component.ts
+++ frontend/src/app/case/case.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-case',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./case.component.css']
 })
 export class CaseComponent {
+  @Input() nbCase: number = 1;
 
 }
--- frontend/src/app/plateau/plateau.component.ts
+++ frontend/src/app/plateau/plateau.component.ts
@@ -16,4 +16,22 @@ export class PlateauComponent {
     this.res = res;
   }
 
+  count(nb: number = 64) {
+    let nbs: {row: number, col: number}[] = []
+    let ax = 0;
+    let ay = 0;
+    for (let i = 0; i < 8; i++) {
+
+    }
+    for (let row = 0; row < 8; row++) {
+      for (let col = 0; col < 8-row; col++) {
+
+      }
+    }
+    return nbs
+  }
+
+  getPositionNb(nb: number, nbCol: number): {x: number, y: number} {
+    return {x: 0, y: 0}
+  }
 }
